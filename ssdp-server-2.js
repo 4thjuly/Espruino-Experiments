c83d1d7a11e103dac629ded4839c1b5aeab88728
Ian Ellison-Taylor <4thjuly@ellison-taylor.com>
2018-03-04 00:34:10 -0800

    Fail: Try test messages

---
+++ ssdp-server-2.js
@@ -0,0 +1,86 @@
+const wifi = require('Wifi');
+const dgram = require('dgram');
+
+const SSID = 'Black';
+const PASSWORD = 'DelayFinish88';
+const UPNP_IP = '239.255.255.250';
+const BROADCAST_IP = '255.255.255.255';
+const TEST_IP = '10.0.0.1';
+const UPNP_PORT = 1900;
+const SSDP_NOTIFY = `NOTIFY * HTTP/1.1 
+HOST: 239.255.255.250:1900
+CACHE-CONTROL: max-age = 1800
+LOCATION: http://127.0.0.0
+NT: upnp:rootdevice
+NTS: ssdp:alive
+SERVER: EspruinoWifi UPnP/1.0 SSDP-Server/1.0
+USN: uuid:f214e5fe-2a1c-42e6-9365-aff5a353547f::upnp:rootdevice
+
+`; // NB Blank preceding bland line
+
+var _ledOn = false;
+var _blinkIntervalID = 0;
+
+wifi.on('connected', () => {
+  console.log('Connected'); 
+  greenLedBlink(false);
+  greenLed(true);
+
+  wifi.getIP((err, d) => { 
+    let ip = d.ip;
+    console.log('Creating socket: ', ip);
+    let srv = dgram.createSocket('udp4');
+    srv.addMembership(UPNP_IP, ip);
+    sendNotify(srv);
+    setInterval(() => { sendNotify(srv); }, 5000);
+  });
+
+});
+
+wifi.on('dhcp_timeout', (details) => {
+  console.log('dhcp_timeout:', details); 
+  greenLed(false);
+});
+
+wifi.on('disconnected', (details) => {
+  console.log('disconnected:', details); 
+  greenLed(false);
+});
+
+function sendNotify(srv) {
+  console.log('Sending...');
+  srv.send(SSDP_NOTIFY, UPNP_PORT, TEST_IP);
+  console.log('Sent');
+}
+
+function greenLedBlink(setOn) {
+  if (setOn) {
+    _blinkIntervalID = setInterval(() => {
+      _ledOn = !_ledOn;
+      digitalWrite(LED2, _ledOn);
+    }, 500);
+  } else {
+    clearInterval(_blinkIntervalID);
+    _blinkIntervalID = 0;
+  }
+}
+
+function greenLed(setOn) {
+  digitalWrite(LED2, setOn);
+}
+
+function onInit() {
+  console.log('connecting...');
+  greenLedBlink(true);
+  wifi.connect(SSID, {password:PASSWORD}, (err) => { 
+    if (!err) { 
+      console.log('Connect success');
+    } else {
+      console.log('Connect error: ', err);
+      greenLedBlink(false);
+      greenLed(false);
+    }
+  });
+}
+
+onInit();
