25a172304730e285609178d716ad9c2d8b9ef656
Ian Ellison-Taylor <4thjuly@ellison-taylor.com>
2018-03-10 16:07:41 -0800

    Fails: trying to reply to message

--- ssdp-server-1.js
+++ ssdp-server-1.js
@@ -1,7 +1,7 @@
 const wifi = require('Wifi');
 const dgram = require('dgram');
 
-const SSID = 'Black';
+const SSID = 'Black-TP-Link_0BF4';
 const PASSWORD = 'DelayFinish88';
 const SSDP_IP = '239.255.255.250';
 const BROADCAST_IP = '255.255.255.255';
@@ -10,10 +10,13 @@ const SSDP_PORT = 1900;
 var _ledOn = false;
 var _blinkIntervalID = 0;
 
-function onMsg(msg, rinfo) {
+function onMsg(socket, msg, rinfo) {
+  console.log('Memory: ', process.memory().free); 
   let type = msg.split(' ')[0];
   if (type === 'M-SEARCH') {
       console.log('Search: ', JSON.stringify(rinfo)); 
+      socket.send('Test reply');
+      socket.close();
   }
 }
 
@@ -27,7 +30,7 @@ wifi.on('connected', () => {
     console.log('Creating socket: ', ip);
     let srv = dgram.createSocket('udp4');
     srv.bind(SSDP_PORT, (res) => {
-      res.on('message', onMsg);
+      res.on('message', (msg, rinfo) => onMsg(srv, msg, rinfo));
       res.on('close', (err) => { console.log('Res Close: ', JSON.stringify(err)); });
     });
   });
@@ -45,6 +48,7 @@ wifi.on('disconnected', (details) => {
 
 function greenLedBlink(setOn) {
   if (setOn) {
+    if (_blinkIntervalID) clearInterval(_blinkIntervalID);
     _blinkIntervalID = setInterval(() => {
       _ledOn = !_ledOn;
       digitalWrite(LED2, _ledOn);
